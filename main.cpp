ae9aa7749b784fecbba404bcf68a05f8b79c083e
Konrad Cielecki <conrisc@gmail.com>
2017-01-02 17:59:10 +0100

    add working threads

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@
 
 using namespace std;
 
+sem_t coutMut;
+
 void *requester(void *file) {
-  //string inputFile = (string)file;
   std::string* inputFile= reinterpret_cast<std::string*>(file);
-  cout<<inputFile<<endl;
+  sem_wait(&coutMut);
+  cout<<*inputFile<<endl;
+  sem_post(&coutMut);
   // while (true) {
   //   #ifdef SLEEP_ENABLED
   //     randsleep();
@@ -67,29 +70,19 @@ void *service(void *sth) {
 
 int main( int argc, char * argv[] ) {
 
+  sem_init(&coutMut, 0, 1);
+
   pthread_t threads[argc-1];
 
   pthread_create(&threads[0], NULL, service, NULL);
 
-  for (long i = 2; i < argc; i++) {
-    pthread_create(&threads[i], NULL, requester, (void *) argv[i]);
+  for (long i = 1; i < argc-1; i++) {
+    string *parm = new string(argv[i+1]);
+    pthread_create(&threads[i], NULL, requester, (void *) parm);
   }
 
-  // sem_init(&toyMut, 0, 1);  //mutex na buffory (wszystkie 3)
-  // sem_init(&gnomes, 0, 1);
-  // sem_init(&santaSem, 0, 0);
-  // sem_init(&gnomeSem[0] ,0, 0);
-  // sem_init(&gnomeSem[1] ,0, 0);
-  // sem_init(&gnomeSem[2] ,0, 0);
-  //
-  //
-  //
-  //
-
-  //
-  //
-  // for (int i = 0; i < 4; i++)
-  //   pthread_join(threads[i], NULL);
+  for (int i = 0; i < argc-1; i++)
+    pthread_join(threads[i], NULL);
 
   return 0;
 }
